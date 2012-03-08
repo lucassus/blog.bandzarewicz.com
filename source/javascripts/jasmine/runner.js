e5fc8050f0f008d73862b749f117de14506b02a7
Lukasz Bandzarewicz <lucassus@gmail.com>
2012-03-08 17:54:04 +0100

    Add jasmine js and css.

---
+++ source/javascripts/jasmine/runner.js
@@ -0,0 +1,28 @@
+// Jasmine spec runner, used in jsfiddles
+window.execJasmine = function() {
+  var jasmineEnv = jasmine.getEnv();
+  jasmineEnv.updateInterval = 1000;
+
+  var trivialReporter = new jasmine.TrivialReporter();
+
+  jasmineEnv.addReporter(trivialReporter);
+
+  jasmineEnv.specFilter = function(spec) {
+    return trivialReporter.specFilter(spec);
+  };
+
+  var currentWindowOnload = window.onload;
+
+  window.onload = function() {
+    if (currentWindowOnload) {
+      currentWindowOnload();
+    }
+    execJasmine();
+  };
+
+  function execJasmine() {
+    jasmineEnv.execute();
+    trivialReporter.outerDiv.className += ' show-passed';
+  }
+
+}
